e0682cab15f83c4c0c095ebd6bea4c5554626be3
Marcus Davies <55892693+marcus-j-davies@users.noreply.github.com>
2021-08-27 10:56:28 +0100

    Update index.ts

--- packages/json/src/index.ts
+++ packages/json/src/index.ts
@@ -5,13 +5,13 @@ export type callback = (info: object) => void;
 export interface transportOptions {
 	level?: string;
 	logCallback: callback;
-};
+}
 
 export class JSONTransport extends Transport {
 	logCallback: callback;
 	public constructor(options:transportOptions) {
 		super({
-			level: options.level || "silly",
+			level: options.level || "silly"
 		});
 		this.logCallback = options.logCallback;
 	}
