aeeff1e8c340fedf49a562861f77b8a942260980
Marcus Davies <55892693+marcus-j-davies@users.noreply.github.com>
2021-08-27 10:52:33 +0100

    Update index.ts

--- packages/json/src/index.ts
+++ packages/json/src/index.ts
@@ -1,24 +1,24 @@
 import type { ZWaveLogInfo } from "@zwave-js/core";
 import Transport from "winston-transport";
 
-export type callback = (info: object) => void
+export type callback = (info: object) => void;
 export interface transportOptions {
-	level?: string,
-	logCallback: callback
-}
+	level?: string;
+	logCallback: callback;
+};
 
 export class JSONTransport extends Transport {
 	logCallback: callback;
 	public constructor(options:transportOptions) {
 		super({
-			level: options.level || 'silly',
+			level: options.level || "silly",
 		});
 		this.logCallback = options.logCallback;
 	}
 
 	public log(info: ZWaveLogInfo, next: () => void): any {
 		const logObject = JSON.parse(JSON.stringify(info));
-		this.logCallback(logObject)
+		this.logCallback(logObject);
 		next();
 	}
 }
